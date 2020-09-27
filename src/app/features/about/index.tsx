a0b0ba929e165bf5b57db7a998a891adaed457fb
ShivamLatawa <shivamlatawa92@gmail.com>
2020-09-27 13:38:57 +0530

    add width to cv

--- src/app/features/about/index.tsx
+++ src/app/features/about/index.tsx
@@ -5,7 +5,7 @@ import cv from '../../../assets/cv.png';
 const About = () => {
     return (
         <StyledContent>
-            <img src={cv} alt="Resume" />
+            <img width="100%" src={cv} alt="Resume" />
         </StyledContent>
     );
 };
